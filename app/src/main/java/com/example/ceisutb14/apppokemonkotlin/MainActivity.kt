6e5de56ec22714b1cddfaf3b90da8e6178db678b
kevinvegah <kevin.vega.h@hotmail.com>
2018-02-20 14:18:23 -0500

    neuvo

--- app/src/main/java/com/example/ceisutb14/apppokemonkotlin/MainActivity.kt
+++ app/src/main/java/com/example/ceisutb14/apppokemonkotlin/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.ceisutb14.apppokemonkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
 
